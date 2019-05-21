72efeff72f1362f6456259aa242ff5c9b2b15201
Vadim Burukin <ism@agrosepmash.ua>
2019-05-21 21:56:32 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by agros on 21.05.2019.
+ */
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> console.log(`Server started on ${PORT}`));
